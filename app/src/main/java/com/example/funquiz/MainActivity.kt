d6f0bb130a5362e96d58c291252c4f557979b34a
MinjaCheng <minja.cheng@iths.se>
2020-09-16 22:07:02 +0200

    created start screen with title and play button

--- app/src/main/java/com/example/funquiz/MainActivity.kt
+++ app/src/main/java/com/example/funquiz/MainActivity.kt
@@ -1,11 +1,20 @@
 package com.example.funquiz
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val pressPlayButton = findViewById<Button>(R.id.button)
+
+        pressPlayButton.setOnClickListener{
+            val intent = Intent(this, PlayActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
