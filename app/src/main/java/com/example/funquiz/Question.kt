7df21186949455c05265799adcc552ff601cd511
MinjaCheng <minja.cheng@iths.se>
2020-09-22 16:34:29 +0200

    Created a new class named Question

---
+++ app/src/main/java/com/example/funquiz/Question.kt
@@ -0,0 +1,12 @@
+package com.example.funquiz
+
+class Question (
+    val id: Int,
+    val question: String,
+    val image: Int,
+    val optionOne: String,
+    val optionTwo: String,
+    val optionThree: String,
+    val correctOption: Int
+)
+
