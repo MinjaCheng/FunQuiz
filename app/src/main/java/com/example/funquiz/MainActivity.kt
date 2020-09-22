d2f4cb7a06e330f5dcd698408c7b686219caf24d
MinjaCheng <minja.cheng@iths.se>
2020-09-22 15:21:28 +0200

    Changed start screen and created question screen

--- app/src/main/java/com/example/funquiz/MainActivity.kt
+++ app/src/main/java/com/example/funquiz/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val pressPlayButton = findViewById<Button>(R.id.button)
+        val pressPlayButton = findViewById<Button>(R.id.playButton)
 
         pressPlayButton.setOnClickListener{
             val intent = Intent(this, PlayActivity::class.java)
---
+++ app/src/main/java/com/example/funquiz/QuestionActivity.kt
@@ -0,0 +1,33 @@
+package com.example.funquiz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class PlayActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_question)
+        
+        val question = findViewById<TextView>(R.id.questionTextView)
+
+        question.text = getString(R.string.questionOne)
+
+        val guessButton1 = findViewById<Button>(R.id.guessButton1)
+
+        guessButton1.text = "25 000 kr"
+
+        val guessButton2 = findViewById<Button>(R.id.guessButton2)
+
+        guessButton2.text = "97 000 kr"
+
+        val guessButton3 = findViewById<Button>(R.id.guessButton3)
+
+        guessButton3.text = "400 000 kr"
+
+
+
+
+    }
+}
