680f15e6b1d4d991665c1fabd50c9f2469854f06
MinjaCheng <minja.cheng@iths.se>
2020-10-14 22:28:03 +0200

    Translate all text to English

--- app/src/main/java/com/example/funquiz/ResultActivity.kt
+++ app/src/main/java/com/example/funquiz/ResultActivity.kt
@@ -1,5 +1,6 @@
 package com.example.funquiz
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,37 +8,38 @@ import android.view.View
 import kotlinx.android.synthetic.main.activity_result.*
 
 class ResultActivity : AppCompatActivity() {
+
+    @SuppressLint("StringFormatMatches")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
 
-        val correctAnswer = intent.getIntExtra(ListOfQuestions.CORRECT_ANSWERS,0)
-        val totalQuestions = intent.getIntExtra(ListOfQuestions.TOTAL_QUESTIONS,0)
+        val correctAnswer = intent.getIntExtra(ListOfQuestions.CORRECT_ANSWERS, 0)
+        val totalQuestions = intent.getIntExtra(ListOfQuestions.TOTAL_QUESTIONS, 0)
 
+        scoreTextView.text = getString(R.string.score_tv, correctAnswer, totalQuestions)
 
-        scoreTextView.text = "$correctAnswer / $totalQuestions"
+        val scorePercent = ((correctAnswer.toFloat() / totalQuestions.toFloat()) * 100).toInt()
 
-        val scorePercent: Int = ((correctAnswer.toFloat()/totalQuestions.toFloat())*100).toInt()
-
-        scorePercentTextView.text = "Du fick $scorePercent % rätt."
+        scorePercentTextView.text = getString(R.string.score_percent_tv, scorePercent)
 
         when {
             scorePercent < 25 -> {
-                credTextView.text = "Ok!"
+                credTextView.text = getString(R.string.cred_tv_ok)
             }
             scorePercent < 50 -> {
-                credTextView.text = "Bra!"
+                credTextView.text = getString(R.string.cred_tv_good)
             }
             scorePercent < 75 -> {
-                credTextView.text = "Jättebra!"
+                credTextView.text = getString(R.string.cred_tv_very_good)
             }
             else -> {
-                credTextView.text = "Utmärkt!"
+                credTextView.text = getString(R.string.cred_tv_excellent)
             }
         }
     }
 
-    fun playAgainButton (view: View) {
+    fun playAgainButton(view: View) {
         val intent = Intent(this, QuestionActivity::class.java)
         startActivity(intent)
         finish()
